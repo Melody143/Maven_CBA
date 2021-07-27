f97e904e4df381002600e2b7044d9a1c4ab02dab
Melody143 <reddymanogna7@gmail.com>
2021-07-27 20:54:33 +0530

    Payment Services in CBA

---
+++ FoodplusCard/src/main/java/Payments/client/2.java
@@ -0,0 +1,10 @@
+package Payments.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class 2 implements EntryPoint {
+  public void onModuleLoad() {
+
+				        				        
+  }
+}
